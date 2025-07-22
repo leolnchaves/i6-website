54fbc29577b4b44be657973e2a7bca476839b323
leolnchaves <leo@infinity6.ai>
2025-07-22 13:30:21 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/i6-site-est/',
   server: {
     host: "::",
     port: 8080,
